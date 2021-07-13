1d89aa4637f3fa4b2da1a70e35632efdb97be1b9
Thbang <Thbang@hyundai-wia.com>
2021-07-13 12:49:01 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import React, {Component} from 'react'
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="gray-background">
+      <header className="App-header">
+        <img src={logo} lat="logo"/>
+        <h2>Let1s develop management sysytem!</h2>
+      </header>
+    </div>
+   ); 
+  
+}
+
+export default App;
