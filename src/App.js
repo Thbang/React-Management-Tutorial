ad0ff969d18b0889f73f3a7e59c0f32df2094ef7
Thbang <Thbang@hyundai-wia.com>
2021-07-14 22:20:57 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -2,7 +2,28 @@ import logo from './logo.svg';
 import React, {Component, PureComponent} from 'react'
 import Customer from './components/Customer'
 import './App.css';
+import Table from '@material-ui/core/Table';
+import Paper from '@material-ui/core/Paper';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+import { render } from '@testing-library/react';
 
+const styles = theme =>({
+root:{
+width:'100%',
+marginTop: theme.spacing.unit*3,
+overflowX:"auto"
+
+
+},
+table:{
+  minWidth:1080
+}
+
+
+})
 const customers = [{
    'id': 1,
    'image': 'http://placeimg.com/64/64/1',
@@ -31,27 +52,35 @@ const customers = [{
  
 }]
 
-function App() {
+class App extends Component {
+  render() {
+    const {classes} = this.props;
+    return (
+      <Paper className = {classes.root}>
+        
+          <Table className = {classes.table}>
+            <TableHead>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableHead>
+            <TableBody>            
+              {customers.map(c =>{return (<Customer key={c.id}  id={c.id} image={c.image}  name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />) }) }
+            </TableBody>
+  
+          </Table>
+        
+  
+       
+  
+      </Paper>
+    );
+  };
 
-  return (
-    <div>
-      {
-        customers.map(c => {
-          return (<Customer 
-            key={c.id}
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birthday={c.birthday}
-            gendaer={c.gender}
-            job={c.job} />)
-        })
-
-      }
-
-    </div>
-  );
   
 }
 
-export default App;
+export default withStyles(styles) (App);
